495cc33926ff1b4a91e7604fc480a49acb07e1ee
collinewait <collinewait17@gmail.com>
2019-05-16 21:27:35 +0300

    initial commit

---
+++ src/main/java/com/example/wait/democopsboot/DemoCopsBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.wait.democopsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCopsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCopsBootApplication.class, args);
+	}
+
+}
