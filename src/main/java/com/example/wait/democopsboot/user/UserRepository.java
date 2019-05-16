e91edbd7b675e901ee2c7b2fcd949d08c43edf5b
collinewait <collinewait17@gmail.com>
2019-05-16 21:37:31 +0300

    create user a repository interface

---
+++ src/main/java/com/example/wait/democopsboot/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.wait.democopsboot.user;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.UUID;
+
+public interface UserRepository extends CrudRepository<User, UUID> {
+}
