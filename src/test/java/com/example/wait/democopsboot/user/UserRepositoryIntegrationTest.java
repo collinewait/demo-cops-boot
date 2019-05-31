8f183936904ed0afe75d1328aa478a98f951aa7c
collinewait <collinewait17@gmail.com>
2019-05-31 15:43:57 +0300

    create an integration test with testContainers

---
+++ src/test/java/com/example/wait/democopsboot/user/UserRepositoryIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.example.wait.democopsboot.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.wait.democopsboot.infrastructure.SpringProfiles;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles(SpringProfiles.INTEGRATION_TEST)
+public class UserRepositoryIntegrationTest {
+	@Autowired
+	private UserRepository repository;
+	@PersistenceContext
+	private EntityManager entityManager;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testSaveUser() {
+		Set<UserRole> roles = new HashSet<>();
+		roles.add(UserRole.OFFICER);
+		User user = repository.save(new User(repository.nextId(),
+				"alex.foley@beverly-hills.com", "my-secret-pwd", roles));
+		assertThat(user).isNotNull();
+
+		assertThat(repository.count()).isEqualTo(1L);
+
+		entityManager.flush();
+		assertThat(jdbcTemplate.queryForObject(
+				"SELECT count(*) FROM copsboot_user", Long.class))
+						.isEqualTo(1L);
+		assertThat(jdbcTemplate
+				.queryForObject("SELECT count(*) FROM user_roles", Long.class))
+						.isEqualTo(1L);
+		assertThat(jdbcTemplate.queryForObject("SELECT roles FROM user_roles",
+				String.class)).isEqualTo("OFFICER");
+	}
+}
